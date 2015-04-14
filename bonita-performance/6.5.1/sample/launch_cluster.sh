aeafb01958c84f7792d6abe8fc178b6a64358beb
Guillaume Rosinosky <guillaumerosinosky@yahoo.fr>
2015-04-14 18:09:40 +0200

    6.5.1 sp performance Dockerfile takes updates from 6.5.x (BENCH_MODE, CLUSTER_MODE) for 6.5.1 stable version

---
+++ bonita-performance/6.5.1/sample/launch_cluster.sh
@@ -0,0 +1,8 @@
+docker run --name mydbpostgres -e POSTGRES_PASSWORD=mysecretpassword -d ci-01.rd.lan:5000/bonitacloud/postgres_9.3_custom
+sleep 30
+docker run --name=bonita-performance_psql -h cloud -v /home/guillaume/svn/cloud/docker/trunk/bonita-performance/6.5.x/sample/bonita:/opt/bonita/ -v /home/guillaume/svn/cloud/docker/trunk/bonita-performance/6.5.x/sample/home:/opt/bonita_home/ -e "BENCH_ACTIVE=true" -e "CLUSTER_MODE=true" -d -p 8081:8080  --link mydbpostgres:postgres bonita_65x_test
+sleep 60
+docker run --name=bonita-performance_psql2 -h cloud -v /home/guillaume/svn/cloud/docker/trunk/bonita-performance/6.5.x/sample/home:/opt/bonita_home/ -e "BENCH_ACTIVE=true" -e "CLUSTER_MODE=true" -d -p 8082:8080  --link mydbpostgres:postgres bonita_65x_test
+sleep 5
+docker run --name=bonita-performance_psql3 -h cloud -v /home/guillaume/svn/cloud/docker/trunk/bonita-performance/6.5.x/sample/home:/opt/bonita_home/ -e "BENCH_ACTIVE=true" -e "CLUSTER_MODE=true" -d -p 8083:8080  --link mydbpostgres:postgres bonita_65x_test
+

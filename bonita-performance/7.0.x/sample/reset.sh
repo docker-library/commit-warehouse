09baae7f357d6e60237bf022e95eebadd12334db
JeremJR <jeremy.jacquier-roux@bonitasoft.com>
2015-08-19 17:34:10 +0200

    add Bonita BPM 7.0.1 Performance

--- bonita-performance/7.0.x/sample/reset.sh
+++
@@ -1,5 +0,0 @@
-docker stop bonita-performance_psql3 bonita-performance_psql2 bonita-performance_psql bonita-performance_h2 mydbpostgres
-docker rm bonita-performance_psql3 bonita-performance_psql2 bonita-performance_psql bonita-performance_h2 mydbpostgres
-rm -Rf bonita/B*
-rm -Rf home/*
-

8ff3cbc5042f60a5399817080d24f5e50e74d1ec
Truc Nguyen <truc.nguyen@bonitasoft.com>
2015-10-20 18:11:52 +0200

    upgrade update script to version 7.1 (was 7.0)

--- update_bonita_or_bonita-performance.sh
+++ update_bonita_or_bonita-performance.sh
@@ -6,64 +6,64 @@ then
 fi
 NEW_RELEASE=$1
 EDITION=$2
-LAST_RELEASE=`grep "^ENV BONITA_VERSION" bonita/7.0/Dockerfile | awk '{ print $3 }'`
-BASE_URL="http://192.168.1.254/qa/releases/7.0.x/${NEW_RELEASE}"
+LAST_RELEASE=`grep "^ENV BONITA_VERSION" bonita/7.1/Dockerfile | awk '{ print $3 }'`
+BASE_URL="http://192.168.1.254/qa/releases/7.1.x/${NEW_RELEASE}"
 
 echo "updating $ed"
 case $EDITION in
 "community")
         BUNDLE="BonitaBPMCommunity-${NEW_RELEASE}-Tomcat-7.0.55"
 	DIR="bonita"
-	LAST_RELEASE=`grep "^ENV BONITA_VERSION" ${DIR}/7.0/Dockerfile | awk '{ print $3 }'`
+	LAST_RELEASE=`grep "^ENV BONITA_VERSION" ${DIR}/7.1/Dockerfile | awk '{ print $3 }'`
 	echo "Last release is : $LAST_RELEASE"
 	echo "We will update to : $NEW_RELEASE"
 	wget ${BASE_URL}/BonitaBPMCommunity-${NEW_RELEASE}/${BUNDLE}.zip -O /tmp/${BUNDLE}.zip
 	SHA256SUM=`sha256sum /tmp/${BUNDLE}.zip | awk '{ print $1 }'`
 	unzip -q /tmp/${BUNDLE}.zip -d /tmp
-	if [ -d "${DIR}/7.0.bck" ]
+	if [ -d "${DIR}/7.1.bck" ]
 	then
-		echo "${DIR}/7.0.bck dir already present, last update seems not terminated, exit"
+		echo "${DIR}/7.1.bck dir already present, last update seems not terminated, exit"
 		exit 1
 	fi
 	echo "backup current version"
-	cp -r ${DIR}/7.0 ${DIR}/7.0.bck
-	sed -i "s/^ENV BONITA_VERSION.*/ENV BONITA_VERSION ${NEW_RELEASE}/" ${DIR}/7.0/Dockerfile
-	sed -i "s/^ENV BONITA_SHA256.*/ENV BONITA_SHA256 ${SHA256SUM}/" ${DIR}/7.0/Dockerfile
-        cp /tmp/${BUNDLE}/bonita/engine-server/conf/platform/bonita-platform-community-custom.properties ${DIR}/7.0/templates/bonita-platform-community-custom.properties
-        cp /tmp/${BUNDLE}/bonita/engine-server/conf/tenants/template/bonita-tenant-community-custom.properties ${DIR}/7.0/templates/bonita-tenant-community-custom.properties
-        cp /tmp/${BUNDLE}/bonita/client/platform/conf/platform-tenant-config.properties ${DIR}/7.0/templates/platform-tenant-config.properties
-        unzip -q -c /tmp/${BUNDLE}/webapps/bonita.war WEB-INF/web.xml > ${DIR}/7.0/files/WEB-INF/web.xml
+	cp -r ${DIR}/7.1 ${DIR}/7.1.bck
+	sed -i "s/^ENV BONITA_VERSION.*/ENV BONITA_VERSION ${NEW_RELEASE}/" ${DIR}/7.1/Dockerfile
+	sed -i "s/^ENV BONITA_SHA256.*/ENV BONITA_SHA256 ${SHA256SUM}/" ${DIR}/7.1/Dockerfile
+        cp /tmp/${BUNDLE}/bonita/engine-server/conf/platform/bonita-platform-community-custom.properties ${DIR}/7.1/templates/bonita-platform-community-custom.properties
+        cp /tmp/${BUNDLE}/bonita/engine-server/conf/tenants/template/bonita-tenant-community-custom.properties ${DIR}/7.1/templates/bonita-tenant-community-custom.properties
+        cp /tmp/${BUNDLE}/bonita/client/platform/conf/platform-tenant-config.properties ${DIR}/7.1/templates/platform-tenant-config.properties
+        unzip -q -c /tmp/${BUNDLE}/webapps/bonita.war WEB-INF/web.xml > ${DIR}/7.1/files/WEB-INF/web.xml
 	rm /tmp/${BUNDLE}.zip
 	rm -rf /tmp/${BUNDLE}
-	meld ${DIR}/7.0.bck ${DIR}/7.0
-	echo "if merge is finished, please remove ${DIR}/7.0.bck directory"
+	meld ${DIR}/7.1.bck ${DIR}/7.1
+	echo "if merge is finished, please remove ${DIR}/7.1.bck directory"
 	;;
 "performance")
         BUNDLE="BonitaBPMSubscription-${NEW_RELEASE}-Tomcat-7.0.55"
         DIR="bonita-performance"
-        LAST_RELEASE=`grep "^ENV BONITA_VERSION" ${DIR}/7.0/Dockerfile | awk '{ print $3 }'`
+        LAST_RELEASE=`grep "^ENV BONITA_VERSION" ${DIR}/7.1/Dockerfile | awk '{ print $3 }'`
 	echo "Last release is : $LAST_RELEASE"
 	echo "We will update to : $NEW_RELEASE"
         wget ${BASE_URL}/BonitaBPMSubscription-${NEW_RELEASE}/${BUNDLE}.zip -O /tmp/${BUNDLE}.zip
         SHA256SUM=`sha256sum /tmp/${BUNDLE}.zip | awk '{ print $1 }'`
         unzip -q /tmp/${BUNDLE}.zip -d /tmp
-        if [ -d "${DIR}/7.0.bck" ]
+        if [ -d "${DIR}/7.1.bck" ]
         then
-                echo "${DIR}/7.0.bck dir already present, last update seems not terminated, exit"
+                echo "${DIR}/7.1.bck dir already present, last update seems not terminated, exit"
                 exit 1
         fi
         echo "backup current version"
-        cp -r ${DIR}/7.0 ${DIR}/7.0.bck
-        sed -i "s/^ENV BONITA_VERSION.*/ENV BONITA_VERSION ${NEW_RELEASE}/" ${DIR}/7.0/Dockerfile
-        sed -i "s/^ENV BONITA_SHA256.*/ENV BONITA_SHA256 ${SHA256SUM}/" ${DIR}/7.0/Dockerfile
-        cp /tmp/${BUNDLE}/bonita/engine-server/conf/platform/bonita-platform-community-custom.properties ${DIR}/7.0/templates/bonita-platform-community-custom.properties
-        cp /tmp/${BUNDLE}/bonita/engine-server/conf/tenants/template/bonita-tenant-community-custom.properties ${DIR}/7.0/templates/bonita-tenant-community-custom.properties
-        cp /tmp/${BUNDLE}/bonita/client/platform/conf/platform-tenant-config.properties ${DIR}/7.0/templates/platform-tenant-config.properties
-	cp /tmp/${BUNDLE}/bonita/engine-server/conf/platform/bonita-platform-sp-custom.properties ${DIR}/7.0/templates/bonita-platform-sp-custom.properties
-        unzip -q -c /tmp/${BUNDLE}/webapps/bonita.war WEB-INF/web.xml > ${DIR}/7.0/files/WEB-INF/web.xml
+        cp -r ${DIR}/7.1 ${DIR}/7.1.bck
+        sed -i "s/^ENV BONITA_VERSION.*/ENV BONITA_VERSION ${NEW_RELEASE}/" ${DIR}/7.1/Dockerfile
+        sed -i "s/^ENV BONITA_SHA256.*/ENV BONITA_SHA256 ${SHA256SUM}/" ${DIR}/7.1/Dockerfile
+        cp /tmp/${BUNDLE}/bonita/engine-server/conf/platform/bonita-platform-community-custom.properties ${DIR}/7.1/templates/bonita-platform-community-custom.properties
+        cp /tmp/${BUNDLE}/bonita/engine-server/conf/tenants/template/bonita-tenant-community-custom.properties ${DIR}/7.1/templates/bonita-tenant-community-custom.properties
+        cp /tmp/${BUNDLE}/bonita/client/platform/conf/platform-tenant-config.properties ${DIR}/7.1/templates/platform-tenant-config.properties
+	cp /tmp/${BUNDLE}/bonita/engine-server/conf/platform/bonita-platform-sp-custom.properties ${DIR}/7.1/templates/bonita-platform-sp-custom.properties
+        unzip -q -c /tmp/${BUNDLE}/webapps/bonita.war WEB-INF/web.xml > ${DIR}/7.1/files/WEB-INF/web.xml
         rm /tmp/${BUNDLE}.zip
         rm -rf /tmp/${BUNDLE}
-        meld ${DIR}/7.0.bck ${DIR}/7.0
-        echo "if merge is finished, please remove ${DIR}/7.0.bck directory"
+        meld ${DIR}/7.1.bck ${DIR}/7.1
+        echo "if merge is finished, please remove ${DIR}/7.1.bck directory"
 	;;
 esac

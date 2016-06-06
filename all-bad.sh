5f523ba784a11e1baf0260194f5427f6b03a588b
Tianon Gravi <admwiggin@gmail.com>
2016-06-06 11:26:52 -0700

    Cleaner/simpler bad ideas thanks to "bashbrew cat"

--- all-bad.sh
+++ all-bad.sh
@@ -3,8 +3,4 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-bashbrew list --all \
-	| cut -d: -f1 \
-	| sort -u \
-	| sed 's!^!https://raw.githubusercontent.com/docker-library/official-images/master/library/!' \
-	| xargs ./bad.sh
+exec ./bad.sh --all

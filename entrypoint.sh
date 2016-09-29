d5f4fadf81a8d3ef5707c9ceda7a071f8e23acc2
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-09-28 19:53:42 -0700

    Fixed classpath.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ if ! command -v -- "$1" >/dev/null 2>&1
 then
   java \
     -Djava.io.tmpdir="$RAPIDOID_TMP" \
-    -cp /app/app.jar \
-    -jar "$RAPIDOID_JAR" \
+    -cp "$RAPIDOID_JAR":/app/app.jar:/app/jars/*.jar org.rapidoid.standalone.Main \
     "$@"
 else
   exec "$@"

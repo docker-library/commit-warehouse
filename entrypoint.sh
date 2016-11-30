4552df257bda6f2f62921e776484c5df3d10553c
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-11-30 21:07:48 +0100

    Using "exec" when executing any process.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 if ! command -v -- "$1" >/dev/null 2>&1
 then
-  java \
+  set -- java \
     -Djava.io.tmpdir="$RAPIDOID_TMP" \
     -cp "$RAPIDOID_JAR":/app/app.jar:/app/jars/*.jar org.rapidoid.standalone.Main \
     "$@"
-else
-  exec "$@"
 fi
+
+exec "$@"

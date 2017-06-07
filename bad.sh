b11d792dcd2c39327ceb1d46268cde6eeb5c64b9
Tianon Gravi <admwiggin@gmail.com>
2017-06-06 21:53:36 -0700

    Add proper multiarch support to bad.sh (pushing ALL supported architecture tags)

--- bad.sh
+++ bad.sh
@@ -6,7 +6,23 @@ template="$(dirname "$(readlink -f "$BASH_SOURCE")")/bad.sh.tmpl"
 export BASHBREW_CACHE="${BASHBREW_CACHE:-${XDG_CACHE_HOME:-$HOME/.cache}/bashbrew}"
 cd "$BASHBREW_CACHE/git"
 
-bashbrew list "$@" \
-	| sed 's!^!https://github.com/docker-library/official-images/raw/master/library/!' \
-	| xargs bashbrew cat -F "$template" \
-	| bash -ex
+set -- $(
+	bashbrew list "$@" \
+		| sed 's!^!https://github.com/docker-library/official-images/raw/master/library/!'
+)
+
+arches=( $(
+	bashbrew cat --format '
+		{{- range .Entries -}}
+			{{- range .Architectures -}}
+				{{- . -}}{{- "\n" -}}
+			{{- end -}}
+		{{- end -}}
+	' "$@" \
+		| sort -u
+) )
+
+for arch in "${arches[@]}"; do
+	bashbrew --arch="$arch" cat -F "$template" "$@" \
+		| bash -ex
+done

f30467bec4b089b4fe8a94c8cc0c717a328d4181
nmihajlovski <nikolce.mihajlovski@gmail.com>
2017-08-14 14:46:51 +0200

    Configured the JVM to be aware of Docker's memory limits.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ if ! command -v -- "$1" >/dev/null 2>&1
 then
   set -- java \
     -Djava.io.tmpdir="$RAPIDOID_TMP" \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:+UseCGroupMemoryLimitForHeap \
     -jar "$RAPIDOID_JAR" \
     "$@"
 fi

48e5b578a10ef4a67b8a02ad17d2ec0063a76c21
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2017-10-26 22:40:27 +0200

    If an application JAR is present, execute it directly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,19 @@ set -e
 
 if ! command -v -- "$1" >/dev/null 2>&1
 then
+
+  if [ -f /app/app.jar ]
+  then
+    JAR='/app/app.jar'
+  else
+    JAR="$RAPIDOID_JAR"
+  fi
+
   set -- java \
     -Djava.io.tmpdir="$RAPIDOID_TMP" \
     -XX:+UnlockExperimentalVMOptions \
     -XX:+UseCGroupMemoryLimitForHeap \
-    -jar "$RAPIDOID_JAR" \
+    -jar "$JAR" \
     "$@"
 fi
 

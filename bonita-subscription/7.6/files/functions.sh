ac2cd7b5d5049d1177b7b5b830652235bafed10d
JeremJR <jeremy.jacquier-roux@bonitasoft.com>
2018-06-26 18:06:21 +0200

    handle also databases names containing @

--- bonita-subscription/7.6/files/functions.sh
+++ bonita-subscription/7.6/files/functions.sh
@@ -62,7 +62,7 @@ function create_database_if_not_exists {
 			# drop database if said so
 			if [ "$db_drop_existing" == "Y" ]
 			then
-				psql -U $db_admin_user -h $db_host -p $db_port -d postgres -c "DROP DATABASE IF EXISTS ${db_name};"
+				psql -U $db_admin_user -h $db_host -p $db_port -d postgres -c "DROP DATABASE IF EXISTS \"${db_name}\";"
 			fi
 
 			# check if the db exists
@@ -70,19 +70,19 @@ function create_database_if_not_exists {
 			# if the db is not present, create it
 			if [ $? -eq 1 ]
 			then
-				psql -U $db_admin_user -h $db_host -p $db_port -d postgres -c "CREATE DATABASE ${db_name} OWNER \"${db_user}\";"
+				psql -U $db_admin_user -h $db_host -p $db_port -d postgres -c "CREATE DATABASE \"${db_name}\" OWNER \"${db_user}\";"
 			fi
 			;;
 		"mysql")
 			# drop database if said so
 			if [ "$db_drop_existing" == "Y" ]
 			then
-				mysql -u $db_admin_user -p${db_admin_pass} -h $db_host --port $db_port -e "DROP DATABASE IF EXISTS ${db_name};"
+				mysql -u $db_admin_user -p${db_admin_pass} -h $db_host --port $db_port -e "DROP DATABASE IF EXISTS \`${db_name}\`;"
 			fi
 
 			# if the db is not present, create it
-			mysql -u $db_admin_user -p${db_admin_pass} -h $db_host --port $db_port -e "CREATE DATABASE IF NOT EXISTS ${db_name};"
-			mysql -u $db_admin_user -p${db_admin_pass} -h $db_host --port $db_port -e "GRANT ALL PRIVILEGES ON ${db_name}.* to '${db_user}'@'%';"
+			mysql -u $db_admin_user -p${db_admin_pass} -h $db_host --port $db_port -e "CREATE DATABASE IF NOT EXISTS \`${db_name}\`;"
+			mysql -u $db_admin_user -p${db_admin_pass} -h $db_host --port $db_port -e "GRANT ALL PRIVILEGES ON \`${db_name}\`.* to '${db_user}'@'%';"
 			;;
 		"oracle")
             ant -f ${BONITA_PATH}/bonita-deployer-databases-${BONITA_DBTOOL_VERSION}/deployer.xml init-oracle \

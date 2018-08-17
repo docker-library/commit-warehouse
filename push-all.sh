8d383324b7ed85aab97d8e6641574d46adac7209
Tianon Gravi <admwiggin@gmail.com>
2018-08-17 12:15:01 -0700

    Parallelize git pushing

--- push-all.sh
+++ push-all.sh
@@ -3,4 +3,8 @@ set -Eeuo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+bashbrew list --repos --all \
+	| sed 's!^!https://github.com/docker-library/official-images/raw/master/library/!' \
+	| xargs -rtn1 -P "$(nproc)" ./push.sh
+
 exec ./push.sh --all

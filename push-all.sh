f528f5eaa7ec2393e66fc3a16716fae45eb34c49
Tianon Gravi <admwiggin@gmail.com>
2018-08-20 14:50:03 -0700

    Remove idiotic extra leftover

--- push-all.sh
+++ push-all.sh
@@ -6,5 +6,3 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 bashbrew list --repos --all \
 	| sed 's!^!https://github.com/docker-library/official-images/raw/master/library/!' \
 	| xargs -rtn1 -P "$(nproc)" ./push.sh
-
-exec ./push.sh --all

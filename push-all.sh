575cdd6c709b0be872c2ad24524f1696a04f754a
Tianon Gravi <admwiggin@gmail.com>
2019-12-31 10:58:13 -0800

    Ditch parallelism

--- push-all.sh
+++ push-all.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 bashbrew list --repos --all \
 	| sed 's!^!https://github.com/docker-library/official-images/raw/master/library/!' \
-	| xargs -rtn1 -P2 ./push.sh
+	| xargs -rtn1 ./push.sh
